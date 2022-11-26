f6cbe2cf37de62a378f37a8537a579324ead5022
sneoo <epicshakil@gmail.com>
2022-11-26 22:05:07 +0600

    added item delete api

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const stripe = require("stripe")(
+  "sk_test_51M66MUG6ZZcJX2lVaINtf8DDgDo8oZeW0Zj3205xe6vre4XkinCQIwLclLGRFhdBi6qv72D0MbqJ1FyocQz9QENl00X3KP02Hp"
+);
 
+const jwt = require("jsonwebtoken");
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -18,6 +22,27 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+
+  const token = authHeader.split(" ")[1];
+  // console.log(token);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    // console.log(err);
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    // console.log(decoded.email);
+    next();
+  });
+}
+
 async function run() {
   try {
     const usersCollection = client.db("kenabecha-app").collection("users");
@@ -28,6 +53,25 @@ async function run() {
     const categoriesCollection = client
       .db("kenabecha-app")
       .collection("categories");
+    const wishlistCollection = client
+      .db("kenabecha-app")
+      .collection("wishlists");
+    const paymentsCollection = client
+      .db("kenabecha-app")
+      .collection("payments");
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACEESS_TOKEN_SECRET, {
+          expiresIn: "7d",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
+    });
 
     app.get("/categories", async (req, res) => {
       const query = {};
@@ -94,6 +138,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/myProducts", async (req, res) => {
+      const id = req.body.id;
+      const query = { _id: ObjectId(id) };
+      const result = await mobilesCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.post("/addProducts", async (req, res) => {
       const mobile = req.body;
       const result = await mobilesCollection.insertOne(mobile);
@@ -107,7 +158,8 @@ async function run() {
 
     app.get("/bookings/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { id: id };
+      console.log(id);
+      const query = { _id: ObjectId(id) };
       const result = await bookingsCollection.findOne(query);
       res.send(result);
     });
@@ -115,6 +167,10 @@ async function run() {
     app.put("/bookings/:id", async (req, res) => {
       const id = req.params.id;
       const booking = req.body;
+      // const bookingEmail = req.body.buyerEmail;
+      // console.log(bookingEmail);
+      console.log(req.body);
+
       const filter = { _id: id };
       const options = { upsert: true };
       const updateDoc = {
@@ -124,11 +180,67 @@ async function run() {
           id: id,
         },
       };
-      const result = await bookingsCollection.updateOne(
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.get("/orders/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const filter = { buyerEmail: email };
+      const result = await bookingsCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    app.post("/dashboard/wishlist/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await mobilesCollection.findOne(query);
+      const postResult = await wishlistCollection.insertOne(result);
+      res.send(result);
+    });
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const order = req.body;
+      const price = order.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+      // console.log(paymentIntent.client_secret);
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    // app.get("/payments", async(req, res) => {
+    //   const query
+    // })
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      console.log(payment);
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.orderId;
+      const filter = { _id: ObjectId(id) };
+      // console.log("id:", id);
+      console.log(filter);
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const updatedResult = await bookingsCollection.updateOne(
         filter,
-        updateDoc,
-        options
+        updatedDoc
       );
+      console.log("updated result", updatedResult);
       res.send(result);
     });
   } finally {
